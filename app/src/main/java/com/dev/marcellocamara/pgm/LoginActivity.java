39187ce5546aa804f0a9f0814fa3fe6721f49d1c
Marcello Câmara <marcellocamara@id.uff.br>
2019-02-05 21:27:43 -0200

    Initial commit

---
+++ app/src/main/java/com/dev/marcellocamara/pgm/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.dev.marcellocamara.pgm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/***
+    marcellocamara@id.uff.br
+            2018
+***/
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
